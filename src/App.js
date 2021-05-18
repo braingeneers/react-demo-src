e6770ffc119e931052ea93f4d6b50d549bac475c
Matthew Elliott <melliot1@ucsc.edu>
2021-05-18 12:55:52 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from '../public/wetai_logo.png';
+//import logo from '../public/wetai_logo.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='wetai_logo.png' className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <b>WE HAVE BEEN CHANGED...</b>
